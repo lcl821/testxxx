9ca4e0bb5f788a3fa25551b1bbfb5847b0f9890f
廖成龙 <liaochenglong@qianduan.com>
2018-10-29 14:23:33 +0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by ucsmy on 2018/10/29.
+//  Copyright © 2018年 ucsmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
